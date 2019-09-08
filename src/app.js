5444e4b3b29cd61b260490f4cfda157fc6e879f6
gitout <martinmurata@gmail.com>
2019-09-08 11:02:18 -0700

    added my name

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(publicDirPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title: 'Weather',
-        name: 'Andres'
+        name: 'Martin Murata'
     })
 })
 app.get('/about',(req,res)=>{
